2a9f5f144c17516276ddf2b60f20e42bb1657d73
Unknown <asyalivshits@yandex.ru>
2017-10-04 22:13:39 +0300

    Tasks

---
+++ livshits_a_b/rational/rational.h
@@ -0,0 +1,51 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+#include <iosfwd>
+#include <iostream>
+
+class Rational
+{
+public:
+    Rational() = default;
+    Rational(const int num) : num_(num) {}
+    Rational::Rational(const int num, const int denum) : num_(num), denum_(denum)
+    {
+        if (denum == 0)
+        {
+            throw std::invalid_argument("Error!");
+        }
+    }
+    void Correct(Rational& t);
+    ~Rational() = default;
+    bool operator==(const Rational& rhs);
+    bool operator!=(const Rational& rhs) { return !operator==(rhs); }
+    Rational& operator+=(const Rational& rhs);
+    Rational& operator+=(const int rhs) { return operator+=(Rational(rhs)); }
+    Rational& operator-=(const Rational& rhs);
+    Rational& operator-=(const double rhs) { return operator-=(Rational(rhs)); }
+    Rational& operator*=(const Rational& rhs);
+    Rational& operator/=(const Rational& rhs);
+    bool operator>(const Rational& rhs);
+    bool operator<(const Rational& rhs);
+
+    std::ostream& writeTo(std::ostream& ostrm) const;
+    std::istream& readFrom(std::istream& istrm);
+
+public:
+    int num_{ 0 };
+    int denum_{ 1 };
+
+    static const char leftBrace{ '{' };
+    static const char separator{ '/' };
+    static const char rightBrace{ '}' };
+};
+inline std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs)
+{
+    return rhs.writeTo(ostrm);
+}
+
+inline std::istream& operator>> (std::istream& istrm, Rational& rhs)
+{
+    return rhs.readFrom(istrm);
+}
+#endif
---
+++ livshits_a_b/rational/rational_test.cpp
@@ -0,0 +1,90 @@
+#include "rational.h"
+#include <iostream>
+#include <sstream>
+bool testParse(const std::string& str)
+{
+    using namespace std;
+    istringstream istrm(str);
+    Rational r;
+    istrm >> r;
+    if (istrm.good())
+    {
+        cout << "Read success: " << str << " -> " << r << endl;
+    }
+    else
+    {
+        cout << "Read error: " << str << " -> " << r << endl;
+    }
+    return istrm.good();
+}
+
+
+int main()
+{
+    using namespace std;
+    Rational r(1, 2);
+    r.Correct(r);
+    cout << r << endl;
+    r += Rational(1, 4);
+    r.Correct(r);
+    cout << r << endl;
+    r -= Rational(2, 3);
+    r.Correct(r);
+    cout << r << endl;
+    r *= Rational(3, 5);
+    r.Correct(r);
+    cout << r << endl;
+    r /= Rational(7, 9);
+    r.Correct(r);
+    cout << r << endl;
+    Rational s(4, 3);
+    Rational k(5, 6);
+    bool h = k > Rational(3, 8);
+    cout << h << endl;
+    bool n = s < Rational(5, 6);
+    cout << n << endl;
+    bool f = s == k;
+    cout << f << endl;
+    bool w = k != Rational(1, 6);
+    cout << w << endl;
+    Rational a(1, 8);
+    Rational* b(&a);
+    Rational c(*b);
+    cout << c << endl;
+    Rational p(4, 5);
+    Rational& u(p);
+    cout << u << endl;
+    testParse("{3/7}");
+    testParse("{0/ 9}");
+    testParse("{3/ 5");
+    return 0;
+}
+std::ostream& Rational::writeTo(std::ostream& ostrm) const
+{
+    ostrm << leftBrace << num_ << separator << denum_ << rightBrace;
+    return ostrm;
+}
+
+std::istream& Rational::readFrom(std::istream& istrm)
+{
+    char leftBrace(0);
+    int num(0);
+    char s(0);
+    int denum(0);
+    char rightBrace(0);
+    istrm >> leftBrace >> num_ >> s >> denum_ >> rightBrace;
+    if (istrm.good())
+    {
+        if ((Rational::leftBrace == leftBrace) && (Rational::separator == s)
+            && (Rational::rightBrace == rightBrace))
+        {
+            num_ = num;
+            denum_ = denum;
+        }
+        else
+        {
+            istrm.setstate(std::ios_base::failbit);
+        }
+    }
+    return istrm;
+}
